a964ab1b3ddf59d8f99f2fdaea24bee168639cd9
zeeshan <syedbhai@gmail.com>
2022-12-08 00:25:25 +0500

    Add the Cancelation Page

---
+++ Akounto.Billing/Presentation/Controllers/Billing/SubscriptionCancelController.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Plan.Database;
+using Plans.Business;
+using Plans.Infrastructure;
+using Plans.Payment.Entities.Billing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akounto.Billing.Presentation.Controllers.Billing
+{
+
+    public class SubscriptionCancelController : Controller
+    {
+    
+        [Route("SubscriptionCancel")]
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+        [Route("SubscriptionCancel/Cancelation")]
+        [HttpPost]
+        public IActionResult Cancelation()
+        {
+            var req = HttpContext.Request;
+            SubscriptionCancelModel response = null;
+            try
+            {
+                response = new SubscriptionCancelModel();
+                response.Description = req.Form["Description"].ToString();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View(response);
+
+        }
+
+    }
+}
---
+++ Plans.Payment/Entities/Billing/SubscriptionCancelModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plans.Payment.Entities.Billing
+{
+  public   class SubscriptionCancelModel
+    {
+        public string Description { get; set; }
+     
+    }
+}
